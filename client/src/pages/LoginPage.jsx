ef267e1d52aa42a2dec44d17a229389ba729a606
Amir Husain <amirhusain4180617@gmail.com>
2025-07-19 22:13:17 +0530

    first

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import assets from '../assets/assets'
+
+const LoginPage = () => {
+  const [currState,setCurrState] = useState("Sign up")
+  const [fullName,setFullName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [bio,setBio] = useState("")
+  const [isDataSubmitted,setIsDataSubmitted] = useState(false)
+  return (
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+
+      {/* left */}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
+      {/* right */}
+      <form  action="" className='border-2 bg-white/8 text-white border-gray-500 p-6 flex '>
+       <h2 className='font-medium text-2xl flex justify-between items-center'>
+        {currState}
+        <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />
+        </h2>
+       
+       {currState === "Sign up" &&  !isDataSubmitted &&(
+          <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder='Full Name' required/>
+       )}
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
+ 
